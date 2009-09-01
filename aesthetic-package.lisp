5435ba5f7ee0fe118447c78ab783055ed3592d44
Rob Myers <robmyers@robs-laptop.(none)>
2009-09-01 22:43:30 +0100

    More package name fixing

--- aesthetic-package.lisp
+++ aesthetic-package.lisp
@@ -15,7 +15,7 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-(defpackage :aesthetic
+(defpackage aesthetic
   (:documentation
    "Aesthetic generation, mutation and evaluation.")
   (:use #:common-lisp)
